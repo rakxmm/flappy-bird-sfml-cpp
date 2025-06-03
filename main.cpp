ed153e01f49a37149a302334bbd1d3dad517a63a
rakxmm <matesfcracek@gmail.com>
2025-06-03 20:44:54 +0200

    Project restarted, because of some issues :D

--- main.cpp
+++ main.cpp
@@ -1,92 +1,66 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 
-class Hampster {
+
+class GameObj {
 public:
-    float width = 40.0f;
-    float height = 40.0f;
-
     sf::Vector2f position;
-    sf::Vector2f velocity = {0.f, 0.f};
-    sf::RectangleShape rectangle;
-    sf::Texture texture;
-    std::unique_ptr<sf::Sprite> sprite;
+    sf::Vector2f size;
+    sf::RectangleShape rect;
 
+    GameObj(float width, float height, sf::Vector2f position, sf::Color color) {
+        this->size = {width, height};
+        this->position = position;
 
-
-    float speed = 300.f;
-    float jump_force = 600.f;
-    float gravity = 9.81f;
-    float friction = 0.92f;
-    float bottom_border = 400.f;
-
-
-
-    Hampster(int x, int y) {
-        this->position.x = x;
-        this->position.y = y;
-
-
-        if (!texture.loadFromFile("images/Hampster.png")) {
-            std::cerr << "Image was not found!!" << std::endl;
-        } else {
-            sprite = std::make_unique<sf::Sprite>(texture);
-            auto textureSize = texture.getSize();
-
-            float scaleX = this->width / textureSize.x;
-            float scaleY = this->height / textureSize.y;
-
-            sprite->setScale({scaleX, scaleY});
-            sprite->setPosition(sf::Vector2f(position));
-        }
-
-        this->rectangle = sf::RectangleShape({this->width, this->width});
-        this->rectangle.setPosition(sf::Vector2f(position));
-        this->rectangle.setFillColor(sf::Color::Blue);
+        this->rect.setSize(this->size);
+        this->rect.setPosition(this->position);
+        this->rect.setFillColor(color);
     }
 
-    void update(float deltaTime) {
-        this->velocity.y += this->gravity;
-        this->position.y += this->velocity.y * deltaTime;
-        this->position.x += this->velocity.x * this->friction * deltaTime;
-        this->velocity.x *= this->friction;
-        if (this->rectangle.getRotation() <= sf::degrees(90)) {
-            this->rectangle.rotate(sf::degrees(100 * deltaTime));
-        }
-
-        if (this->position.y >= this->bottom_border) {
-            this->velocity.y = 0;
-            this->position.y = this->bottom_border;
-        }
-
-        this->rectangle.setPosition(sf::Vector2f(this->position));
+    void draw(sf::RenderWindow& window) const {
+        window.draw(this->rect);
     }
 
-    bool collide(const sf::Vector2i a) const {
-        return a.x >= this->position.x && a.x <= this->position.x + this->width && a.y >= this->position.y && a.y <= this->position.y + this->width;
+    bool collide(const GameObj& obj) const {
+        return  obj.position.x + obj.size.x >= this->position.x &&
+                obj.position.x <= this->position.x + this->size.x &&
+                obj.position.y + obj.size.y >= this->position.y &&
+                obj.position.y <= this->position.y + this->size.y;
     }
-
-    void jump() {
-        if (this->position.y > 0) {
-            this->rectangle.setRotation(sf::degrees(10));
-            this->velocity.y = -this->jump_force;
-        }
-    }
-
-    void move(bool move_left) {
-        if (move_left) {
-            this->velocity.x = -this->speed;
-        } else {
-            this->velocity.x = this->speed;
-        }
-    }
-
-    void draw(sf::RenderWindow& window) {
-        if (sprite) {
-            window.draw(*sprite);
-        }
-    };
 };
+
+
+class Player : public GameObj {
+public:
+    float speed = 300.f;
+    float friction = 0.99f;
+
+    Player(sf::Vector2f position)
+        : GameObj(50, 50, position, sf::Color::Blue)
+    {
+
+    }
+
+
+    void move_up(float deltaTime) {
+        this->position.y -= speed * deltaTime * friction;
+    }
+
+    void move_down(float deltaTime) {
+        this->position.y += speed * deltaTime * friction;
+    }
+    void move_left(float deltaTime) {
+        this->position.x -= speed * deltaTime * friction;
+    }
+    void move_right(float deltaTime) {
+        this->position.x += speed * deltaTime * friction;
+    }
+
+    void update() {
+        this->rect.setPosition(this->position);
+    }
+};
+
 
 
 
@@ -97,44 +71,43 @@ int main() {
     sf::Clock clock;
     sf::Time last = clock.getElapsedTime();
 
-
-    Hampster bird(100, 100);
-
     bool fly_released = true;
     int fps = 0;
+
+    Player p({100, 100});
+    GameObj o2(100, 100, {200, 100}, sf::Color::Red);
+
     while (window.isOpen()) {
         float deltaTime = clock.restart().asSeconds();
         while (auto event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
-
-
-
-
         }
 
 
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W) && fly_released) {
-            bird.jump();
+
             fly_released = false;
         } else if (not sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
             fly_released = true;
         }
 
-
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+            p.move_up(deltaTime);
+        }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
-            bird.move(false);
+            p.move_right(deltaTime);
         }
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
-            bird.move(true);
+            p.move_left(deltaTime);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
+            p.move_down(deltaTime);
         }
 
-
-
-        bird.update(deltaTime);
-
+        window.clear();
 
         sf::Time current = clock.getElapsedTime();
         fps++;
@@ -144,9 +117,16 @@ int main() {
             last = current;
         }
 
-        window.clear();
-        bird.draw(window);
 
+        o2.draw(window);
+        p.draw(window);
+
+        if (p.collide(o2)) {
+            std::cout << "Collided" << std::endl;
+        }
+
+
+        p.update();
         window.display();
 
     }
